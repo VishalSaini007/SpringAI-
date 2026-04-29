b06eeb287401424285472385a41fc0b4b1c746e9
vishal saini <vishalsaini9582@gmail.com>
2026-04-29 23:45:55 +0530

    adding ChatClient with Controller

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    private final ChatClient chatClient;
+
+    public HelloController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping
+    public String prompt (@RequestParam String message )
+    {
+        return chatClient
+                .prompt(message)
+                .call()
+                .content();
+
+    }
+}
